bbdcc6a8dffbe497453a1d167519c963fe2e0299
josekurian87 <40492282+josekurian87@users.noreply.github.com>
2023-03-04 13:08:08 +0530

    Hooks

--- Home.tsx
+++ Home.tsx
@@ -3,6 +3,10 @@ import './style.css';
 
 export default function Home() {
   let [count, setCount] = React.useState(0);
+  React.useEffect(() => {
+    console.log('useEffect()');
+    return () => console.log('cleanUp()');
+  }, [count]);
   return (
     <div>
       <h1 onClick={() => setCount(count + 1)}>Hello StackBlitz${count}!</h1>
